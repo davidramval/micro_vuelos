e7c665bbf3ee132471c12b9b6d0bfcffda1a4b80
david <cdramirez050@misena.edu.co>
2020-11-15 10:28:42 -0500

    Se elimina proyecto

--- src/main/java/com/semillero/aerolinea/repository/IVueloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.semillero.aerolinea.repository;
-
-import com.semillero.aerolinea.entity.VueloEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IVueloRepository extends JpaRepository<VueloEntity,Integer> {
-}
